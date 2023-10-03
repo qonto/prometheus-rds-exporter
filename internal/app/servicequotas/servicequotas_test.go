2bd3779e65af063ade866639531eca529c850aed
Vincent Mercier <vmercier@gmail.com>
2023-10-03 23:11:36 +0200

    refact(servicequotes): Use mockery for tests

--- internal/app/servicequotas/servicequotas_test.go
+++ internal/app/servicequotas/servicequotas_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	aws_servicequotas "github.com/aws/aws-sdk-go-v2/service/servicequotas"
 	aws_servicequotas_types "github.com/aws/aws-sdk-go-v2/service/servicequotas/types"
 	"github.com/qonto/prometheus-rds-exporter/internal/app/servicequotas"
+	"github.com/qonto/prometheus-rds-exporter/internal/app/servicequotas/mocks"
 	converter "github.com/qonto/prometheus-rds-exporter/internal/app/unit"
 
 	"github.com/stretchr/testify/assert"
@@ -17,34 +19,21 @@ import (
 const (
 	UnknownServiceQuota       = float64(42)
 	DBinstancesQuota          = float64(10)
-	totalStorage              = float64(10)
-	manualDBInstanceSnapshots = float64(42)
+	totalStorage              = float64(24)
+	manualDBInstanceSnapshots = float64(100)
 )
 
-// mockServiceQuotasClient defines a mock for the AWS service quotas
-type mockServiceQuotasClient struct{}
-
-func (m mockServiceQuotasClient) GetServiceQuota(context context.Context, input *aws_servicequotas.GetServiceQuotaInput, optFns ...func(*aws_servicequotas.Options)) (*aws_servicequotas.GetServiceQuotaOutput, error) {
-	value := UnknownServiceQuota
-
-	if *input.ServiceCode == servicequotas.RDSServiceCode {
-		switch *input.QuotaCode {
-		case servicequotas.DBinstancesQuotacode:
-			value = DBinstancesQuota
-		case servicequotas.TotalStorageQuotaCode:
-			value = totalStorage
-		case servicequotas.ManualDBInstanceSnapshotsQuotaCode:
-			value = manualDBInstanceSnapshots
-		}
-	}
-
-	quota := &aws_servicequotas_types.ServiceQuota{Value: &value}
-
-	return &aws_servicequotas.GetServiceQuotaOutput{Quota: quota}, nil
-}
-
 func TestGetRDSQuotas(t *testing.T) {
-	mockClient := mockServiceQuotasClient{}
+	mockClient := mocks.NewServiceQuotasClient(t)
+
+	dbInstanceResponse := aws_servicequotas.GetServiceQuotaOutput{Quota: &aws_servicequotas_types.ServiceQuota{Value: aws.Float64(DBinstancesQuota)}}
+	mockClient.On("GetServiceQuota", context.TODO(), &aws_servicequotas.GetServiceQuotaInput{QuotaCode: aws.String(servicequotas.DBinstancesQuotacode), ServiceCode: aws.String("rds")}).Return(&dbInstanceResponse, nil).Once()
+
+	totalStorageResponse := aws_servicequotas.GetServiceQuotaOutput{Quota: &aws_servicequotas_types.ServiceQuota{Value: aws.Float64(totalStorage)}}
+	mockClient.On("GetServiceQuota", context.TODO(), &aws_servicequotas.GetServiceQuotaInput{QuotaCode: aws.String(servicequotas.TotalStorageQuotaCode), ServiceCode: aws.String("rds")}).Return(&totalStorageResponse, nil).Once()
+
+	manualSnapshotResponse := aws_servicequotas.GetServiceQuotaOutput{Quota: &aws_servicequotas_types.ServiceQuota{Value: aws.Float64(manualDBInstanceSnapshots)}}
+	mockClient.On("GetServiceQuota", context.TODO(), &aws_servicequotas.GetServiceQuotaInput{QuotaCode: aws.String(servicequotas.ManualDBInstanceSnapshotsQuotaCode), ServiceCode: aws.String("rds")}).Return(&manualSnapshotResponse, nil).Once()
 
 	result, err := servicequotas.NewFetcher(mockClient).GetRDSQuotas()
 	require.NoError(t, err, "GetRDSQuotas must succeed")
