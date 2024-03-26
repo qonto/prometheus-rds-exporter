99d01b41378a612119a0ead26a71571b4239a180
Vincent Mercier <vmercier@gmail.com>
2024-03-26 10:03:11 +0100

    refact(ec2): Use table tests
    Signed-off-by: Vincent Mercier <vmercier@gmail.com>

--- internal/app/ec2/ec2_test.go
+++ internal/app/ec2/ec2_test.go
@@ -15,18 +15,46 @@ func TestGetDBInstanceTypeInformation(t *testing.T) {
 	context := context.TODO()
 	client := mock.EC2Client{}
 
+	testCases := []struct {
+		instanceType      string
+		vCPU              int32
+		memory            int64
+		maximumIops       int32
+		maximumThroughput float64
+	}{
+		{
+			instanceType:      "t3.large",
+			vCPU:              mock.InstanceT3Large.Vcpu,
+			memory:            converter.MegaBytesToBytes(mock.InstanceT3Large.Memory),
+			maximumIops:       mock.InstanceT3Large.MaximumIops,
+			maximumThroughput: converter.MegaBytesToBytes(mock.InstanceT3Large.MaximumThroughput),
+		},
+		{
+			instanceType:      "t3.small",
+			vCPU:              mock.InstanceT3Small.Vcpu,
+			memory:            converter.MegaBytesToBytes(mock.InstanceT3Small.Memory),
+			maximumIops:       mock.InstanceT3Small.MaximumIops,
+			maximumThroughput: converter.MegaBytesToBytes(mock.InstanceT3Small.MaximumThroughput),
+		},
+	}
+	expectedAPICalls := float64(1)
+
 	instanceTypes := []string{"db.t3.large", "db.t3.small"}
 	fetcher := ec2.NewFetcher(context, client)
 	result, err := fetcher.GetDBInstanceTypeInformation(instanceTypes)
 
 	require.NoError(t, err, "GetDBInstanceTypeInformation must succeed")
-	assert.Equal(t, mock.InstanceT3Large.Vcpu, result.Instances["db.t3.large"].Vcpu, "vCPU don't match")
-	assert.Equal(t, converter.MegaBytesToBytes(mock.InstanceT3Large.Memory), result.Instances["db.t3.large"].Memory, "Memory don't match")
-	assert.Equal(t, mock.InstanceT3Large.MaximumIops, result.Instances["db.t3.large"].MaximumIops, "MaximumThroughput don't match")
-	assert.Equal(t, converter.MegaBytesToBytes(mock.InstanceT3Large.MaximumThroughput), result.Instances["db.t3.large"].MaximumThroughput, "MaximumThroughput don't match")
+	assert.Equal(t, expectedAPICalls, fetcher.GetStatistics().EC2ApiCall, "EC2 API calls don't match")
 
-	assert.Equal(t, mock.InstanceT3Small.Vcpu, result.Instances["db.t3.small"].Vcpu, "vCPU don't match")
-	assert.Equal(t, converter.MegaBytesToBytes(mock.InstanceT3Small.Memory), result.Instances["db.t3.small"].Memory, "Memory don't match")
+	for _, tc := range testCases {
+		testName := "Test " + tc.instanceType
+		t.Run(testName, func(t *testing.T) {
+			instance := result.Instances["db."+tc.instanceType]
 
-	assert.Equal(t, float64(1), fetcher.GetStatistics().EC2ApiCall, "EC2 API call don't match")
+			assert.Equal(t, tc.vCPU, instance.Vcpu, "vCPU don't match")
+			assert.Equal(t, tc.memory, instance.Memory, "Memory don't match")
+			assert.Equal(t, tc.maximumIops, instance.MaximumIops, "Maximum IOPS don't match")
+			assert.Equal(t, tc.maximumThroughput, instance.MaximumThroughput, "Maximum throughput don't match")
+		})
+	}
 }
